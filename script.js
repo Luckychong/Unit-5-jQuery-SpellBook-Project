4f3cde93c9ed7a5b66162fa08614b5ff5b9c1546
Luckychong <43589909+Luckychong@users.noreply.github.com>
2019-02-26 16:25:56 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+//Spell #1
+
+$(".fire").click(function(){ 
+     $("#flareon").toggle();
+ 
+});
+
+
+
+//Spell #2
+
+$(".water").click(function(){
+    $("#vaporeon").fadeToggle();
+ 
+});
+
+
+
+//Spell #3
+
+$(".thunder   ").click(function(){
+    $("#jolteon").slideToggle();
+ 
+});
+
+
+
